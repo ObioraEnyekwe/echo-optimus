0e0c38cf4fb1341c28eef4e501c037f6989f37d1
ObioraEnyekwe <149326868+ObioraEnyekwe@users.noreply.github.com>
2024-03-30 09:09:58 -0600

    Delete main_mar29.tf

--- main_mar29.tf
+++
@@ -1,9 +0,0 @@
-resource "random_string" "naming" {
-  special = false
-  upper   = false
-  length  = 20
-}
-
-locals {
-  prefix = var.prefix != "" ? var.prefix : "capstone-demo${random_string.naming.result}"
-}
