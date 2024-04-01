13786d01cf062d71419e2fe55cc4d3f52cc89431
ehicode <ehicode@gmail.com>
2024-04-01 06:24:45 +0000

    add new terraform files

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+databricks_account_username = "okoerikigsley1@gmail.com"
+databricks_account_password = "f5Da?5yLET88V5+"
+databricks_account_id       = "061892dd-5ca6-46b8-9a52-abba669b6b59"
+
---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "databricks_account_id" {
+  type        = string
+  description = "Databricks Account ID"
+}
+
+variable "tags" {
+  default     = {}
+  type        = map(string)
+  description = "Optional tags to add to created resources"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block of VPC"
+  type        = string
+  default     = "192.0.0.0/25"
+}
+
+variable "public_subnets_cidr" {
+  type    = list(string)
+  default = ["192.0.0.0/26"]
+}
+
+variable "privatelink_subnets_cidr" {
+  type    = list(string)
+  default = ["192.0.0.64/27", "192.0.0.96/27"]
+}
+variable "availability_zones" {
+  type    = list(string)
+  default = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
+}
+
+
+variable "region" {
+  default     = "ca-central-1"
+  type        = string
+  description = "AWS region to deploy to"
+}
+
+variable "prefix" {
+  default     = "capstone-proj"
+  type        = string
+  description = "Prefix for use in the generated names"
+}
+
+
+
+variable "workspace_1_config" {
+  default = {
+    private_subnet_pair = { subnet1_cidr = "192.0.0.64/27", subnet2_cidr = "192.0.0.64/27" }
+    workspace_name      = "test-workspace-1"
+    prefix              = "ws1" // prefix decides subnets name
+    region              = "ca-central-1"
+    root_bucket_name    = "test-workspace-1-rootbucket"
+    block_list          = ["58.133.93.159"]
+    allow_list          = [] // if allow_list empty, all public IP not blocked by block_list are allowed
+    tags = {
+      "Name" = "test-workspace-1-tags",
+      "Env"  = "test-ws-1"
+    }
+  }
+}
+
